03c17a2f6081fb7b6cfd47088f2710a62b2e45b6
Jonathan Tien <jonathan.tien@gmail.com>
2014-06-20 14:58:22 -0700

    added up to note 26

---
+++ _annotations/25.m
@@ -0,0 +1,29 @@
+---
+layout: default
+title: Brother Dong Rationalizes Replacing Brother Lee's Speaking with His Own 
+ref: 25
+---
+
+Brother Dong claimed that he was forced to replace Brother Lee's speaking with his own because Brother Lee's messages were too deep for the Brazilian saints to grasp.
+
+Dong Yu Lan, International Conference at Estância Árvore da Vida, February 17, 2007, Message 1:
+
+> In 1975, a group of young people were brought into the church life through the practice of calling on the Lord. When they called, they were in the spirit, they grew in life, and their life was significantly changed. Long mustaches were gone and long hair cut short... Then I led them into the life-study in 1975. I took them to the Life-study training in the US... The life-study contains words of life, but we should not read it in a doctrinal way; rather, we should receive it as rhema that we may have life. Those young men who went were unable to fully grasp the meaning, so I re-spoke Br. Lee's words. Instead of simply repeating what he said, which they had already heard, I digested the messages and converted logos to rhema, thus leading them into life. We are not studying the letters in the Bible, but rather we are drawing out the spirit from the word. It is the Spirit who gives life. Therefore, since 1975, all the churches [in Brazil] have been practicing only these two things: calling on the Lord and pray-reading the word.
+
+Brother Dong even published such a claim in print in Daily Food: The Organic Study of John's Writings, Week 1, Saturday:
+
+> The young people we took to the trainings were not able to fully absorb the Lord's burden in the word because the messages were too rich. Then I began to ruminate the messages and pass them on to them. I began to digest and re-digest them in order for the logos word to become the rhema word, that is, for it to be the word the Lord speaks to us personally, in order to bring these young people into the word of life. In this way the messages were not mere doctrine, the black and white letter of the Bible, but became Spirit and life.
+
+Brother Dong's assessment that "the young people we took to the trainings were not able to fully absorb the Lord's burden in the word because the messages were too rich" is in direct contradiction to Brother Lee's own appraisal:
+
+> I must tell you, it was a wonderful time! It shocked me, by what? By the ability, the capacity of all the attendants to receive such, not only a deeper truth, I would say, the deepest truth. I must tell you that through my over 50 years of speaking, this message, I mean, this topic was the ever deeper topic. No topic I ever had picked up was so deep as this one. Yet, you couldn't believe those Brazilian saints, my, they just devoured, and swallowed, and digested. From their sharings, from their reviews, I fully realized they got into these things. I really marveled at their receiving ability and capacity. (A Report Given by Brother Lee on His Visit to Brazil," given in Anaheim, California, March 25, 1984)
+
+Brothers who were close co-workers of Brother Dong see this as the beginning of the deviation of Brother Dong from closely following the ministry in the Lord's recovery.
+
+Statement from Fernando Monte-Serrat, October 26, 2007:
+
+> As the years went by, especially from 1986, Brother Dong little by little, began to deviate from the ministry. Brother Dong began to say that the South Americans have no capacity to understand the high teachings of Brother Lee, and that he had to digest and transmit those teachings in a lower level for the saints to understand. This is how Brother Dong explained his practice to "filter" the words of the ministry, depriving the saints from the healthy food that produces the building up of the Body of Christ.
+
+Statement from David Franco, June 18, 2008:
+
+> At first Brother Dong excused the change in content both in public and in private by saying that the word ministered in the trainings was too high and consequently too difficult to understand, and that therefore he needed to digest the messages first and then adapt them to the local and regional situations. This, he said, would allow the saints to be nourished. The contents of his summer and winter training messages were completely different from the messages given in Anaheim, and even strange in their content. Because many of us knew English and some of us also attended the trainings in Anaheim, we became very aware of these differences in the messages. This situation caused most of the saints, however, to lose direct contact with the word of the ministry.
